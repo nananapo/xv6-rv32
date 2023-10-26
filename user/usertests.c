cdf597f4ec9f68f1769a23bd6e83a4da0c886355
kanataso <kanapo.go@gmail.com>
2023-10-26 17:31:53 +0900

    memcpyを追加

--- user/usertests.c
+++ user/usertests.c
@@ -22,6 +22,15 @@
 char buf[BUFSZ];
 char name[3];
 
+void *memcpy(void *dest, const void *src, unsigned int n)
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
+        ((char*)dest)[i] = ((char*)src)[i];
+    }
+    return dest;
+}
+
 // does chdir() call iput(p->cwd) in a transaction?
 void
 iputtest(char *s)
